248c5961004bab83d2b6b6a1d06089dfa147899d
ButtFaceMcCrack <thepaleone@gmail.com>
2020-01-22 22:35:13 -0500

    Add capture button to tsk

--- multibox-typescript/src/App.tsx
+++ multibox-typescript/src/App.tsx
@@ -2,25 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+declare const chrome: any;
+
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const captureStream = () => {
+
+        chrome.runtime.sendMessage(chrome.runtime.getManifest().id, 'capture', {}, async (args: any) => {
+            let stream;
+
+            const constraints = {
+                audio: true,
+                video: true,
+                chromeMediaSourceId: args.sourceId
+            };
+
+            stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+            const video = document.getElementById('screen-view');
+            // @ts-ignore
+            video.src = URL.createObjectURL(stream);
+        });
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <video id={"display_output"}></video>
+                <button onClick={captureStream}>Capture Stream</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
