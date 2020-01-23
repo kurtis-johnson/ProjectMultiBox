e32078ab585a9c060c59575f54b394424e35234c
Matthew Campbell <matthew.campbell@getconga.com>
2020-01-22 21:47:15 -0500

    Change capture button to launch app btn

--- multibox-chrome-ext/popup.js
+++ multibox-chrome-ext/popup.js
@@ -1,14 +1,21 @@
-var captureButton = document.getElementById('capture_stream_button');
+// var captureButton = document.getElementById('capture_stream_button');
+//
+// captureButton.onclick = function(element) {
+//     chrome.runtime.sendMessage(chrome.runtime.getManifest().id, 'capture', {}, function (args) {
+//         // chrome.windows.create({
+//         //     url: 'playback_window.html',
+//         //     type: 'popup',
+//         //     width: 800,
+//         //     height: 600
+//         // }, function(displayWindow) {
+//         //     displayWindow['showStream'](args.streamId);
+//         // });
+//     });
+// };
 
-captureButton.onclick = function(element) {
-    chrome.runtime.sendMessage(chrome.runtime.getManifest().id, 'capture', {}, function (args) {
-        // chrome.windows.create({
-        //     url: 'playback_window.html',
-        //     type: 'popup',
-        //     width: 800,
-        //     height: 600
-        // }, function(displayWindow) {
-        //     displayWindow['showStream'](args.streamId);
-        // });
+var launch_app_btn = document.getElementById('launch_app');
+launch_app_btn.onclick = function(element) {
+    chrome.tabs.create({
+        url: 'index.html'
     });
 };
