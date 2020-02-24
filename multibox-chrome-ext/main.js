66a7fb8844bed86b176c827d29a223f5b140e011
Matthew Campbell <matthew.campbell@getconga.com>
2020-02-23 21:04:56 -0500

    added some notes

--- multibox-chrome-ext/main.js
+++ multibox-chrome-ext/main.js
@@ -1,3 +1,13 @@
 chrome.runtime.onStartup.addListener(function (args) {
     console.log(args);
 });
+
+function muteTab(tab) {
+    chrome.tabs.update(tab.id, {muted: true});
+}
+
+function unmuteTab(tab) {
+    chrome.tabs.update(tab.id, {muted: false});
+}
+
+// https://github.com/ludiosarchive/mute-new-tabs TODO: MUTE ALL THE TABS
